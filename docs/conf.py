c18f47ca6bb527e4c413adc80be31d7c96bd5ce4
jptngames <jpgames.jns@gmail.com>
2025-08-24 13:35:13 -0400

    não gosto de configurar essa bomba

--- docs/conf.py
+++ docs/conf.py
@@ -7,47 +7,41 @@ import re
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.append(os.path.abspath('extensions'))
 
-project = 'discord.py'
-author = 'Rapptz'
-copyright = '2015-present, Rapptz'
+# -- Configurações básicas ------------------------------------------------
+project = "discord.py"
+author = "Rapptz"
+release = "1.0"
 
-version = ''
-with open('../discord/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
-release = version
-branch = 'master' if version.endswith('a') else 'v' + version
-
-# -- General configuration ---------------------------------------------------
+# -- Extensões do Sphinx --------------------------------------------------
 extensions = [
-    'builder',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon',
-    'sphinxcontrib_trio',
-    'sphinx_inline_tabs',
-    'details',
-    'exception_hierarchy',
-    'attributetable',
-    'resourcelinks',
-    'nitpick_file_ignorer',
-    'colour_preview',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib_trio",
+    "sphinx_inline_tabs",
 ]
 
+# Ordem de membros e typehints
 autodoc_member_order = 'bysource'
 autodoc_typehints = 'none'
 
+# Extlinks (links rápidos)
 extlinks = {
     'issue': ('https://github.com/Rapptz/discord.py/issues/%s', 'GH-%s'),
     'ddocs': ('https://discord.com/developers/docs/%s', None),
 }
 
+# Intersphinx
 intersphinx_mapping = {
-  'py': ('https://docs.python.org/3', None),
-  'aio': ('https://docs.aiohttp.org/en/stable/', None),
-  'req': ('https://requests.readthedocs.io/en/latest/', None)
+    'py': ('https://docs.python.org/3', None),
+    'aio': ('https://docs.aiohttp.org/en/stable/', None),
+    'req': ('https://requests.readthedocs.io/en/latest/', None)
 }
 
+# RST prolog (substituições para português)
 rst_prolog = """
 .. |coro| replace:: Esta função é uma |coroutine_link|_.
 .. |maybecoro| replace:: Esta função *pode ser uma* |coroutine_link|_.
@@ -55,87 +49,66 @@ rst_prolog = """
 .. _coroutine_link: https://docs.python.org/3/library/asyncio-task.html#coroutine
 """
 
-templates_path = ['_templates']
-source_suffix = '.rst'
-master_doc = 'index'
+# -- Templates e paths ----------------------------------------------------
+templates_path = ["_templates"]
+exclude_patterns = ['_build']
 
-# Idioma em PT-BR
+# -- Idioma ---------------------------------------------------------------
 language = "pt_BR"
-
 locale_dirs = ['locale/']
 gettext_compact = False
 
-exclude_patterns = ['_build']
-pygments_style = 'friendly'
+# -- HTML -----------------------------------------------------------------
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_title = "discord.py (Documentação em Português)"
 
-nitpick_ignore_files = [
-  "migrating_to_async",
-  "migrating_to_v1",
-  "migrating",
-  "whats_new",
-]
+# Contexto adicional
+html_context = {
+    'discord_invite': 'https://discord.gg/r3sSKJJ',
+    'discord_extensions': [
+        ('discord.ext.commands', 'ext/commands'),
+        ('discord.ext.tasks', 'ext/tasks'),
+    ],
+}
 
 # Filtro para warnings inúteis de tradução
 def _i18n_warning_filter(record: logging.LogRecord) -> bool:
-  return not record.msg.startswith(
-    (
-      'inconsistent references in translated message',
-      'inconsistent term references in translated message',
+    return not record.msg.startswith(
+        (
+            'inconsistent references in translated message',
+            'inconsistent term references in translated message',
+        )
     )
-  )
 _i18n_logger = logging.getLogger('sphinx')
 _i18n_logger.addFilter(_i18n_warning_filter)
 
-# -- Options for HTML output -------------------------------------------------
-html_experimental_html5_writer = True
-html_theme = 'sphinx_rtd_theme'
-
-html_context = {
-  'discord_invite': 'https://discord.gg/r3sSKJJ',
-  'discord_extensions': [
-    ('discord.ext.commands', 'ext/commands'),
-    ('discord.ext.tasks', 'ext/tasks'),
-  ],
-}
-
-resource_links = {
-  'discord': 'https://discord.gg/r3sSKJJ',
-  'issues': 'https://github.com/Rapptz/discord.py/issues',
-  'discussions': 'https://github.com/Rapptz/discord.py/discussions',
-  'examples': f'https://github.com/Rapptz/discord.py/tree/{branch}/examples',
-}
-
-html_favicon = './images/discord_py_logo.ico'
-html_static_path = ['_static']
-
-html_search_scorer = '_static/scorer.js'
+# Arquivos JavaScript adicionais
 html_js_files = [
-  'custom.js',
-  'settings.js',
-  'copy.js',
-  'sidebar.js'
+    'custom.js',
+    'settings.js',
+    'copy.js',
+    'sidebar.js'
 ]
 
-htmlhelp_basename = 'discord.pydoc'
+# Favicon
+html_favicon = './images/discord_py_logo.ico'
 
-# -- Options for LaTeX output ------------------------------------------------
+# -- Opções de saída adicionais (LaTeX, manual, Texinfo) -----------------
 latex_elements = {}
 latex_documents = [
-  ('index', 'discord.py.tex', 'discord.py Documentação',
-   'Rapptz', 'manual'),
+    ('index', 'discord.py.tex', 'discord.py Documentação',
+     'Rapptz', 'manual'),
 ]
 
-# -- Options for manual page output ------------------------------------------
 man_pages = [
-    ('index', 'discord.py', 'discord.py Documentação',
-     ['Rapptz'], 1)
+    ('index', 'discord.py', 'discord.py Documentação', ['Rapptz'], 1)
 ]
 
-# -- Options for Texinfo output ----------------------------------------------
 texinfo_documents = [
-  ('index', 'discord.py', 'discord.py Documentação',
-   'Rapptz', 'discord.py', 'Uma linha de projeto.',
-   'Miscellaneous'),
+    ('index', 'discord.py', 'discord.py Documentação',
+     'Rapptz', 'discord.py', 'Uma linha de projeto.',
+     'Miscellaneous'),
 ]
 
 def setup(app):
